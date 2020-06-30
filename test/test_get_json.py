16fcb01a54d9649e618b66d77774a25e0d759e88
ryanfitz514 <ryanfitz514@gmail.com>
2020-06-30 14:57:41 -0400

    Added unit tests for get_json

---
+++ test/test_get_json.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.insert(0,".")
+from modules import get_json
+import unittest
+import os
+
+class TestGetJson(unittest.TestCase):
+
+    def test_write_to_json_file(self):
+        get_json.write_to_json_file(os.getcwd(),"test", {'test':'yes'})
+        bool = os.path.isfile(os.path.join(os.getcwd(),'test.json'))
+        self.assertTrue(bool)
+        os.remove(os.path.join(os.getcwd(), 'test.json'))
+
+    def test_authenticate(self):
+        result = get_json.authenticate()
+        self.assertTrue((len(result) > 10) and (isinstance(result, str)))
+
+    def test_get_docket(self):
+        result = get_json.get_docket("17-645","Supreme Court of the United States")
+        self.assertTrue(result['success'])
+
+    def test_format_case_number(self):
+        result = get_json.format_case_number("16-2013-CF-006932-AXXX-MA")
+        self.assertEqual(result, "2013-CF-006932")
+
+
+if __name__ == '__main__':
+    unittest.main()
