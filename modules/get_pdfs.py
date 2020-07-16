9a3b18f37af316debc2708e05bf9cec06f821134
ryanfitz514 <ryanfitz514@gmail.com>
2020-07-16 12:39:14 -0400

    Removed dated, unused code from get_pdfs.py

--- modules/get_pdfs.py
+++ modules/get_pdfs.py
@@ -4,9 +4,9 @@ import json
 from multiprocessing import Pool, cpu_count
 import re
 import datetime
-import threading # development
-import time # development
-import concurrent.futures # development
+import threading
+import time
+import concurrent.futures
 # Third-party Modules
 from progress.bar import IncrementalBar
 from tqdm import tqdm
@@ -16,7 +16,7 @@ import PyPDF2 #DEV
 from config import config
 from modules import file_browser, global_variables
 import gui #DEV
-from modules import log_errors_to_table #DEV
+from modules import log_errors_to_table
 from modules import login
 
 # Reinforces that the variables defined in the global_variables module, and then edited from within other modules,
@@ -81,6 +81,12 @@ def get_urls(input_directory):
     # Loops through every file in the 'result' directory.
     for file in os.listdir(input_directory):
 
+        # We store our output path for PDFs from our global variable in a local variable.
+        # The output path must be included in the result because when the resulting tuples get passed to our
+        # download_from_link_list() function within the thread_download_pdfs() wrapper function, it can't access the 
+        # global variable directly from inside the seperate threads.
+        PDF_OUTPUT_PATH = global_variables.PDF_OUTPUT_PATH
+
         # Tells the progress bar to move forward on each iteration of the loop/
         # bar.next()
 
@@ -126,7 +132,7 @@ def get_urls(input_directory):
 
                         link_filename = f"{docNum} - {docName}"
 
-                        link_tuple = (link, link_filename, base_filename)
+                        link_tuple = (link, link_filename, base_filename, PDF_OUTPUT_PATH)
                         # Add the found link to the list, which will ultimately be returned at the end of the function.
                         pdf_list.append(link_tuple)
 
@@ -153,7 +159,7 @@ def get_urls(input_directory):
                                 # We package the name, link, and filename together in a tuple, that will be passed as an argument to our
                                 # download_from_link_list() function within the multiprocess_download_pdfs() function where we use imap to
                                 # downloading with multiprocessing functionality.
-                                exhibitLink_tuple = (exhibitLink, exhibitName, base_filename)
+                                exhibitLink_tuple = (exhibitLink, exhibitName, base_filename), PDF_OUTPUT_PATH
                                 pdf_list.append(exhibitLink_tuple)
 
             # We close the file when we are done. This also ensures that the file is saved.    
@@ -188,17 +194,13 @@ def download_from_link_list(link_list):
             # Then, create it!
             os.makedirs(outputDirectoryPath)
     
-    # We then make an http request to the pdf link and save the result in a variable.
-
-    #############################################################################################
-
-    params = {"login_token": user.authenticate()} ### DEV ###
-
-
-    result = requests.get(link, stream=True, params=params) ### DEV (params = params) ###
-
-    ##############################################################################################
     
+    # We ready our authentication token to pass as a paramater with our http request to get the pdf file. You must be logged in to access the files.
+    params = {"login_token": user.authenticate()}
+
+    # We then make an http request to the pdf link and save the result in a variable. We pass the authentication token as a parameter.
+    result = requests.get(link, stream=True, params=params)
+
     try:
         result.raise_for_status() #DEV
     
@@ -225,58 +227,6 @@ def download_from_link_list(link_list):
     
     return
 
-    # try:
-    #     PyPDF2.PdfFileReader(open(outputFilePath, "rb"))
-    # except PyPDF2.utils.PdfReadError as a:
-    #     timeNow = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y")
-    #     with open(os.path.join('log', 'log.txt'), 'a') as errorlog:
-    #         errorlog.write(f"\n{timeNow}\n")
-    #         errorlog.write(f"{a}")
-    #         errorlog.write(f"\n{link}\n{fileName}\n{folderName}\n{outputPath}\n------------------")
-    #     with lock:
-    #         tableErrorLog.append_error_table(f"{a}", folderName, fileName)
-
-
-
-
-def multiprocess_download_pdfs(link_list):
-    """
-    Gets a list of PDF documents with appropriate output filenames and foldernames from the get_urls() function.
-    We use multiprocessing to dedicate all of our cpu cores to running the download_from_link_list() function with the
-    result of get_urls as the arguments for each pass through the function.
-    """
-
-    print("Downloading PDFs...")
-
-    print(global_variables.PDF_OUTPUT_PATH)
-
-    # The list of tuples we get in link_list will be passed as arguments when calling download_from_link_list() with imap below to
-    # use multiprocessing. There is an issue where the function cannot find global variables when it is called from within imap.
-    # We use the add_path_to_list_of_tuples() function to append each tuple in the list with the correct file path from the global variable
-    # which is specified by the user within the menus. This ensures that the correct output path can be passed as an argument.
-    link_list = add_path_to_list_of_tuples(link_list, global_variables.PDF_OUTPUT_PATH)
-
-    # Initiates the pool, assigns different calls to a function to a specified amount of CPU cores.
-    # In this case, we pass the cpu_count() which represents the amount of CPU cores in the current system.
-    # This will ensure that all the systems cores get utilized, and the more cores the system has, the faster the files will download.
-    with Pool(cpu_count()) as pool:
-        # Starmap allows us to pass multiple arguments to the function of our choice. The arguments are fed in as a list of tuples.
-        # download_from_link_list is the function we are passing the tuples to.
-        # Files will not be downloaded in order, they are simply downloaded. When a process finishes on one core, it simply picks up
-        # another call to the function with the tuple arguments that haven't been passed through yet.
-        try:
-            pool.starmap(download_from_link_list, link_list)
-        except Exception as a:
-
-            # If there is an error, the log file located at log\log.txt will be appended with the date and time, followed by the error that occured.
-            # The program will not stop if an error is thrown.
-            timeNow = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y")
-            with open(os.path.join('log', 'log.txt'), 'a') as errorlog:
-                errorlog.write(f"\n{timeNow}\n")
-                errorlog.write(a)
-                
-    # When the job is finished. We terminate the pool and free-up our cpu cores.
-    pool.terminate()
 
 def thread_download_pdfs(link_list):
     """
@@ -288,15 +238,10 @@ def thread_download_pdfs(link_list):
     # amount of downloads as a parameter
     maximum = len(link_list)
 
+    print("Downloading PDF files...")
+
     # Starts a timer, we end the timer after we run the function with threading to see how long the bulk download
     # took in total.
-    
-
-    # We append the output path the user chose that was assigned to a global variable to the list of tuples that will
-    # be passed in as arguments. We do this because functions run inside threads have a hard time accessing the changed
-    # value in global_variables.py
-    link_list = add_path_to_list_of_tuples(link_list, global_variables.PDF_OUTPUT_PATH)
-    print("Downloading PDF files...")
     start = time.perf_counter()
     # We start up the threading executor
     with concurrent.futures.ThreadPoolExecutor() as executor:
