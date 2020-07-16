a7afe10482193d80a728b979efc738a5f674604f
ryanfitz514 <ryanfitz514@gmail.com>
2020-07-16 12:13:30 -0400

    Json downloads now use threading instead of multiprocessing

--- modules/get_json.py
+++ modules/get_json.py
@@ -2,11 +2,16 @@
 import re           # For pattern matching within strings
 import json         # For working with json data
 import os           # For accessing features of the operating system
+import threading
+import concurrent.futures
+import datetime
+import time
 # Third-party Modules
 from progress.bar import IncrementalBar    # For showing graphical loading bars
 from retrying import retry                 # For automatically retrying code that throws errors
 import requests                            # For making http requests
 import pandas as pd                        # For working with tabular data
+from tqdm import tqdm
 # Internal Modules
 from config import config
 from modules import login, file_browser, global_variables
@@ -45,49 +50,11 @@ def write_to_json_file(fileName, data):
         input()
         print(e)
 
-def authenticate():
-    """
-    Returns the authentication token to make API calls.
-    Make sure that auth.py is filled out!
-    """
-    user = login.Credentials()
-
-    # This is the endpoint for logging in to Docket Alarm from the API.
-    login_url = "https://www.docketalarm.com/api/v1/login/"
-
-    # The data we will send to the endpoint with our post request will be
-    # our Docket Alarm username and password.
-    data = {
-        'username':user.username,
-        'password':user.password,
-        'client_matter': user.client_matter,
-        }
-
-    # We save the response to a variable. The response is a json object containing
-    # our authentication key iside the json key named 'login_token'
-    result = requests.post(login_url, data=data)
-
-    # Calling the .json() method on the result turns it into a python dictionary we
-    # can work with natively.
-    result_json = result.json()
-
-    if result_json['success'] != True:
-        print("\n\nThere was an error authenticating your account.\nPlease check config/auth.py and make sure your Docket Alarm account info is there.\nReference the documentation for more information.\n")
-        input()
-        print(result_json['error'])
-
-    # We go into the 'login_token' key in our dictionary. The value we save here to
-    # this variable is our authentication key.
-    login_token = result_json['login_token']
-
-    # We have the function return the key so this function can be called wherever we need
-    # the key.
-    return login_token
 
 @retry
 def get_docket(docket, caseCourt):
     """ 
-    Takes in a docket number as an argument and returns all the JSON data available from Docket Alarm.
+    Takes in a docket number and the name of the court the docket is in as arguments and returns all the JSON data available from Docket Alarm.
     """
 
     # We create a user object from the /sav/credentials.pickle file so we can access the username and password from its attributes.
@@ -99,7 +66,7 @@ def get_docket(docket, caseCourt):
     # The parameters we pass to the endpoint. This is how the API knows how to find what we are looking for.
     data = {
         # The token generated after logging in.
-        'login_token':authenticate(),
+        'login_token': user.authenticate(),
         # The reason for use
         'client_matter': user.client_matter,
         # The court we want to search.
@@ -112,9 +79,11 @@ def get_docket(docket, caseCourt):
         'normalize':True,
     }
 
+    headers = {'Content-Type':'application/x-www-form-urlencoded'}
+
     # Makes the api call. We specify the endpoint and the parameters as arguments. The results of the API call are returned and
     # stored to a variable. 
-    result = requests.get(getdocket_url, data, timeout=30)
+    result = requests.get(getdocket_url, data, timeout=30, headers=headers)
     
     # We use .json() to convert the json results to a python dictionary we can more easily work with.
     result_json = result.json()
@@ -147,7 +116,7 @@ def loop_dataframe():
     """
     progress = 0 #DEV
 
-    # Opens a file browser where the user can use a friendly GUI to find their CSV input file.
+    # Gets the path to the input csv that the user specified in the menu.
     spreadsheet_path = global_variables.CSV_INPUT_PATH
 
     main.clear()
@@ -219,3 +188,105 @@ def loop_dataframe():
         # In case the users OS doesn't support this feature, we except any errors.
         pass  
 
+
+def table_to_list_of_tuples():
+    spreadsheet_path = global_variables.CSV_INPUT_PATH
+    JSON_INPUT_OUTPUT_PATH = global_variables.JSON_INPUT_OUTPUT_PATH
+    output_list_of_tuples = []
+
+    try:
+        df = pd.read_csv(spreadsheet_path)
+        len_df = len(df)
+    except Exception as e:
+        print(f"{e}")
+        input()
+    
+ 
+    for index, row in df.iterrows():
+        caseName = row[0]
+        caseNo = row[1]
+        caseCourt = row[2]
+        row_tuple = (caseName, caseNo, caseCourt, JSON_INPUT_OUTPUT_PATH)
+        output_list_of_tuples.append(row_tuple)
+    
+    return output_list_of_tuples
+
+@retry
+def download_json_from_list_of_tuples(result_tuple):
+
+    caseName, caseNo, caseCourt, JSON_INPUT_OUTPUT_PATH = result_tuple
+
+    user = login.Credentials()
+
+    # The endpoint we will be connecting to. Calls to this endpoint return the json data for the docket we want.
+    getdocket_url = "https://www.docketalarm.com/api/v1/getdocket/"
+
+    # The parameters we pass to the endpoint. This is how the API knows how to find what we are looking for.
+    data = {
+        # The token generated after logging in.
+        'login_token': user.authenticate(),
+        # The reason for use
+        'client_matter': user.client_matter,
+        # The court we want to search.
+        'court': caseCourt,
+        # The docket number we want data for
+        'docket':caseNo,
+        # A boolean representing whether or not we want the cached version of the data.
+        'cached': config.isCached,
+        # Cleans up names
+        'normalize':True,
+    }
+
+    # Makes the api call. We specify the endpoint and the parameters as arguments. The results of the API call are returned and
+    # stored to a variable. 
+    result = requests.get(getdocket_url, data, timeout=100)
+
+    
+
+    try:
+        result.raise_for_status() #DEV
+    except:
+        result_json = None
+        print(result)
+        print(caseName)
+        print(caseNo)
+        timeNow = datetime.datetime.now().strftime("%I:%M%p %B %d, %Y")
+        with open(os.path.join('log', 'log.txt'), 'a') as errorlog:
+            errorlog.write(f"\n{timeNow}\n")
+            errorlog.write("JSON could not be downloaded:\n")
+            errorlog.write(f"{result}: {caseName}, {caseNo}, {caseCourt}\n")
+            errorlog.write("------------------")
+        return
+    result_json = result.json()
+    # We use .json() to convert the json results to a python dictionary we can more easily work with.
+
+    
+    try:
+        # Created the path where our .json file will be saved to
+        # filePathNameWExt = os.path.join(config.cwd, folder, fileName + '.json')
+        filePathNameWExt = os.path.join(JSON_INPUT_OUTPUT_PATH, f"{caseName} {caseNo}" + '.json')
+
+
+        # When 'opening' a file that doesn't yet exist, we create that file.
+        # Here, we create the json file we'll be saving the data to.
+        with open(filePathNameWExt, 'w') as fp:
+
+            # Then we write the data to the newly created .json file.
+            json.dump(result_json,fp)
+
+    except Exception as e:
+        print("\nError writing json file.\nReference the documentation for more information\n")
+        input()
+        print(e)
+    return
+
+def thread_download_json():
+    tuples_from_table = table_to_list_of_tuples()
+    maximum = len(tuples_from_table)
+    print("Downloading JSON files...")
+    start = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(download_json_from_list_of_tuples, tuples_from_table), total=maximum))
+    finish = time.perf_counter()
+    print(f"Finished downloading JSON files in {round(finish-start)} seconds.")
+    return results
